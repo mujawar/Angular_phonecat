22350d02c8b9c55ed140d87fe8d60fceb533c34f
arif mujawar <arifmr91@gmail.com>
2017-01-23 22:54:48 +0530

    Update controllers.js

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,6 @@
 'use strict';
 
 /* Controllers */
-
 var phonecatControllers = angular.module('phonecatControllers', []);
 
 phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
@@ -10,12 +9,12 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
     $scope.orderProp = 'age';
   }]);
 
+
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
   function($scope, $routeParams, Phone) {
     $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
       $scope.mainImageUrl = phone.images[0];
     });
-
     $scope.setImage = function(imageUrl) {
       $scope.mainImageUrl = imageUrl;
     }
